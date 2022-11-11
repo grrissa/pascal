4863738b810811545c0af1939357d80146fc8e9a
Gabe Krishnadasan <gkrishnadasan@sandiego.edu>
2022-11-11 12:39:17 -0800

    Docstring for battleship

--- battleship.py
+++ battleship.py
@@ -1,3 +1,12 @@
+"""
+Description: Class for battleship object
+<pre>
+Name: Gabriel Krishnadasan, Alizea Hinz, Aidan Rooney, Marissa Nicole Esteban (Pascal)
+Course: COMP-305 FA22
+Professor: A. Nuzen
+</pre>
+"""
+
 import ship
 class battleship(ship):
     def __init__(self, length:int) -> None:
