9f9f438c96736801c27efe82419a58b1d65abd1a
Gabe Krishnadasan <gkrishnadasan@sandiego.edu>
2022-11-11 12:43:29 -0800

    docstring for ship

--- ship.py
+++ ship.py
@@ -1,3 +1,12 @@
+"""
+Description: Class for ship object
+<pre>
+Name: Gabriel Krishnadasan, Alizea Hinz, Aidan Rooney, Marissa Nicole Esteban (Pascal)
+Course: COMP-305 FA22
+Professor: A. Nuzen
+</pre>
+"""
+
 class ship(object):
     def __init__(self, length:int) -> None:
         self.length = length
