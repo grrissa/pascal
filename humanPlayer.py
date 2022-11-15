f4bc5b5d4e9003eff485e7f7b73dc758e9d59ff7
Alizea Hinz <ahinz@sandiego.edu>
2022-11-15 14:53:45 -0800

    cells are now clickable

--- humanPlayer.py
+++ humanPlayer.py
@@ -1,9 +1,10 @@
-import player, gameboard
+import player, gameboard, cell
 
 class humanPlayer(player):
-    def __init__(self, score:int, is_turn:bool)->None:
+    def __init__(self, score:int, is_turn:bool, board:cell[][])->None:
         self.score = score
-        self.is_turn = False
+        self.is_turn = is_turn
+        self.board = board
 
     def make_hit(self)->None:
         #Need to implement
