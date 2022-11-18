f9067b1df4670fc74fe304a971b1fef9fa892381
Gabe Krishnadasan <gkrishnadasan@sandiego.edu>
2022-11-17 17:36:31 -0800

    Re push

--- computerPlayer.py
+++ computerPlayer.py
@@ -8,10 +8,27 @@ Professor: A. Nuzen
 """
 
 from player import player
+from cell import cell
 class computerPlayer(player):
-    def __init__(self, score:int, is_turn:bool)->None:
-        self.score = score
-        self.is_turn = is_turn
+    def __init__(self, playerNum, score:int, is_turn:bool)->None:
+        super().__init__(playerNum, score, is_turn)
+
+        shipCells = []
+        for r in range(10):
+            row = []
+            for c in range(10):
+                row.append(cell(False, False, "NA"))
+            shipCells.append(row)
+
+        attackingCells = []
+        for r in range(10):
+            row = []
+            for c in range(10):
+                row.append(cell(False, False, "NA"))
+            attackingCells.append(row)
+        
+        self.shipCells = shipCells
+        self.attackingCells = attackingCells
 
     def make_hit(self)->None:
         #Need to implement
